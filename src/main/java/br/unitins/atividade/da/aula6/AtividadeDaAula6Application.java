2c22ba9318055b0959c0edab315c7195e4bbb642
Cleuane Santos <clewsantos09@gmail.com>
2025-04-27 21:00:34 -0300

    Commit inicial com arquivos já existentes

---
+++ src/main/java/br/unitins/atividade/da/aula6/AtividadeDaAula6Application.java
@@ -0,0 +1,13 @@
+package br.unitins.atividade.da.aula6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtividadeDaAula6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtividadeDaAula6Application.class, args);
+	}
+
+}
